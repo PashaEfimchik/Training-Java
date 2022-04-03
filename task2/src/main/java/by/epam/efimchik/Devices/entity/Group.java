41f1483ecb75a50064e84d894371779cc68fe85f
PashaEfimchik <61446424+PashaEfimchik@users.noreply.github.com>
2022-04-04 01:44:07 +0300

    Delete task2 directory

--- task2/src/main/java/by/epam/efimchik/Devices/entity/Group.java
+++
@@ -1,6 +0,0 @@
-package by.epam.efimchik.Devices.entity;
-
-public enum Group {
-    IO_DEVICE,
-    MULTIMEDIA
-}
--- task2/src/main/java/by/epam/efimchik/Devices/entity/Port.java
+++
@@ -1,9 +0,0 @@
-package by.epam.efimchik.Devices.entity;
-
-public enum Port {
-    COM,
-    USB,
-    LPT,
-    HDMI,
-    NO_PORT
-}
